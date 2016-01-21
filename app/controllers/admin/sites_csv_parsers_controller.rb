8ac87d42596e2ce2d9b1c1b98dfef5b757c9fdda
Ben Robinson <benjaminrobinson1313@gmail.com>
2016-01-21 11:43:18 +0800

    Importing sites with CSV feature finished

--- app/controllers/admin/sites_csv_parsers_controller.rb
+++ app/controllers/admin/sites_csv_parsers_controller.rb
@@ -7,11 +7,13 @@ class Admin::SitesCsvParsersController < Admin::BaseController
   def create
     authorize(SitesCsvParser)
 
-    if params[:file].blank? || !params[:file].content_type.include?("csv")    
+    if params[:csv_parser][:file].blank? || !params[:csv_parser][:file].content_type.include?("csv")    
       flash[:alert] = "You must upload a CSV file"
     else
       flash.clear
-      @csv_parser = SitesCsvParser.new(params[:file])
+
+      uploaded_file = params[:csv_parser][:file]
+      @csv_parser = SitesCsvParser.new(uploaded_file.tempfile)
     end
   
     render :new
