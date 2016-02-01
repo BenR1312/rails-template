4101bf6f3a64f3ed107de1a0ea1432e48e49cbe2
Ben Robinson <benjaminrobinson1313@gmail.com>
2016-02-01 14:08:52 +0800

    Update documentation

--- app/models/fleet.rb
+++ app/models/fleet.rb
@@ -9,6 +9,11 @@ class Fleet < ActiveRecord::Base
   validates_associated :fleet_trucks
   validates :fleet_name, presence: true
 
+  # We need to manually ensure the uniqueness of Truck ids because when a Fleet is being
+  # created it will not have an id for the FleetTruck object to validate against
+  #
+  # This call must be under the validates_associated(:fleet_trucks), because that validation
+  # will remove any errors we manually add to the FleetTruck objects
   validate :unique_truck_ids
 
 private
@@ -20,11 +25,12 @@ private
 
   def unique_truck_ids
     if has_duplicate_truck_ids?
-      # adds error message for truck
+      # Ensure error message will be visible of Fleet form
       fleet_trucks.each do |fleet_truck|
         fleet_truck.errors.add(:truck, "You cannot assign the same Truck multiple times")
       end
-      # required to call error on trucks, because of associations
+      # Since validates_associated will be called before this validation, we need to force
+      # this Fleet to be invalid by setting and error
       errors.add(:fleet_trucks, "You cannot assign the same Truck multiple times")
     end
   end
