8cd87bbcc60a3111795f208db4b269dee76303c1
Ben Robinson <benjaminrobinson1313@gmail.com>
2016-02-02 13:55:51 +0800

    Uploader for images

---
+++ app/controllers/admin/companies_controller.rb
@@ -0,0 +1,54 @@
+class Admin::CompaniesController < Admin::BaseController
+
+  def index
+    authorize(Company)
+    @company = Company.all
+  end
+
+  def new
+    @company = Company.new
+    authorize(@company)
+  end
+
+  def create
+    @company = Company.new(company_params)
+    authorize(@company)
+    @company.save
+
+    respond_with(@company, location: admin_companies_path)
+  end
+
+  def show
+    authorize(@company)
+    @company = Company.find(params[:id])
+  end
+
+  def edit
+    @company = Company.find(params[:id])
+    authorize(@company)
+
+    respond_with(@company, location: admin_companies_path)
+  end
+
+  def update
+    @company = Company.find(params[:id])
+    @company.assign_attributes(company_params)
+    authorize(@company)
+
+    @company.save
+    respond_with(@company, location: admin_companies_path)
+  end
+
+  def destroy
+    @company = Company.find(params[:id])
+    authorize(@company)
+    @company.destroy
+    respond_with(@company, location: admin_companies_path)
+  end
+
+private
+
+  def company_params
+    params.require(:company).permit(:name, :country, :slogan, {banner_image: []}, {logo: []}, sponsors_attributes: [:id, :company_id, :name, {logo: []}, :description, :_destroy] )
+  end
+end
