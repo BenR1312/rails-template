4fb7162fd2a4834c2a41ae47824e8e5e2f9cb54e
Ben Robinson <benjaminrobinson1313@gmail.com>
2016-02-05 11:15:55 +0800

    Companies will now render default image if no image is uploaded

--- app/uploaders/banner_image_uploader.rb
+++ app/uploaders/banner_image_uploader.rb
@@ -1,2 +1,6 @@
 class BannerImageUploader < BaseUploader
+
+  def default_url
+    "default.png"
+  end
 end
--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -1,2 +1,6 @@
 class LogoUploader < BaseUploader
+
+  def default_url(*args)
+    "default.png"
+  end
 end
