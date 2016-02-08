1245e6081679b476d84b329ae7fe439fd58ead2e
Ben Robinson <benjaminrobinson1313@gmail.com>
2016-02-08 10:31:35 +0800

    Logo_uploader now handles images correctly and can display default thumbnails

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -1,13 +1,15 @@
 class LogoUploader < BaseUploader
   include CarrierWave::MiniMagick
 
-  process resize_to_fit: [250, 250]
+  version :logo do 
+    process resize_to_fit: [250, 250]
+  end
 
   version :thumb do
     process resize_to_fill: [50,50]
   end
 
   def default_url
-    "logo_placeholder.png"
+    "/assets/fallback/" + [version_name, "logo_placeholder.png"].compact.join('_')
   end
 end
