22a18101389e07180ab5ac3169174a54dd4538de
Ben Robinson <benjaminrobinson1313@gmail.com>
2016-02-09 13:22:02 +0800

    Spec now passes correctly

--- spec/carrier_wave_support.rb
+++ spec/carrier_wave_support.rb
@@ -8,7 +8,7 @@ RSpec.configure do |config|
   config.after(:all) do
     BaseUploader.enable_processing = true
 
-    uploaded_files_path = BaseUploader.base_dir
+    uploaded_files_path = BaseUploader.store_dir
     FileUtils.rm_r(uploaded_files_path) if File.exists?(uploaded_files_path)
   end
 
