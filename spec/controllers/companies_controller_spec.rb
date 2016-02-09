139c40c2cac83d10581069cff699087ae1297439
Ben Robinson <benjaminrobinson1313@gmail.com>
2016-02-09 13:24:39 +0800

    Spec correctly passes

--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe CompaniesController do
+  let(:company) { FactoryGirl.create :company }
 
   describe 'GET show' do
-    subject { get :show, id: company.id }
+    subject { get :show, params }
+    let(:params) { { id: company.id} }
     it { should render_template(:show) }
   end
 
